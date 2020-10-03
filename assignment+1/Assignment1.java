abd3fa5c0ae3165a6520fd3c943dbf145218dddc
Sarabraj Singh <singh.sarabraj@gmail.com>
2020-10-02 22:18:08 -0400

    refactored stacked-linkedlist implementation
    refactored implementation as a generic

--- assignment+1/Assignment1.java
+++ assignment+1/Assignment1.java
@@ -1,13 +1,13 @@
 public class Assignment1 {
 
-    static class LinkedListStack {
+    static class LinkedListStack<T> {
         // an inner class to define a single linked list node
-        private class Node {
-            String value;
-            Node next;
+        private class Node<T> {
+            T value;
+            Node<T> next;
         }
         // keep track of the head of the stack, head.next will point to the next element in the stack
-        private Node head;
+        private Node<T> head;
 
         // constructor for the stack class
         public LinkedListStack(){
@@ -15,33 +15,50 @@ public class Assignment1 {
         }
 
         // pop implementation
-        public String pop() {
+        public T pop() {
             if(head == null){
                 throw new CustomException("empty stack");
             }
 
-            String value = head.value;
+            T value = head.value;
             head = head.next;
+
             return value;
         }
 
         // push implementation
-        public void push(String value){
-            Node oldHead = this.head;
-            this.head = new Node();
+        public void push(T value){
+            Node<T> oldHead = this.head;
+            this.head = new Node<>();
             this.head.value = value;
             this.head.next = oldHead;
         }
     }
     public static void main(String[] args){
-        LinkedListStack lls = new LinkedListStack();
+
+        long startTime = System.currentTimeMillis();
+
+        LinkedListStack<String> lls = new LinkedListStack<>();
+
         lls.push("YO");
         lls.push("WADDUP");
 
         System.out.println("pop1=" + lls.pop());
         System.out.println("pop2=" + lls.pop());
-        System.out.println("pop3=" + lls.pop());
+        //System.out.println("pop3=" + lls.pop());
 
+        LinkedListStack<Integer> iLls = new LinkedListStack<>();
+        iLls.push(15);
+        iLls.push(20);
+        iLls.push(25);
+
+        System.out.println("pop4=" + iLls.pop());
+        System.out.println("pop5=" + iLls.pop());
+        System.out.println("pop6=" + iLls.pop());
+
+        long endTime = System.currentTimeMillis();
+
+        System.out.println(endTime-startTime);
     }
     
     static class CustomException extends RuntimeException {

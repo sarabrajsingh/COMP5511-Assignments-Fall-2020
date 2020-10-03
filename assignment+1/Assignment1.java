6fa8ac964d188a92a6d0f3d3c29b5ad60adbd887
Sarabraj Singh <singh.sarabraj@gmail.com>
2020-10-02 23:30:17 -0400

    linted

--- assignment+1/Assignment1.java
+++ assignment+1/Assignment1.java
@@ -10,28 +10,28 @@ public class Assignment1 {
             T value;
             Node next;
         }
-        // keep track of the head of the stack, head.next will point to the next element in the stack
+
+        // keep track of the head of the stack, head.next will point to the next element
+        // in the stack
         private Node head;
 
         // constructor for the stack class
-        public LinkedListStack(){
+        public LinkedListStack() {
             this.head = null;
         }
 
         // pop implementation
         public T pop() {
-            if(head == null){
+            if (head == null) {
                 throw new CustomException("empty stack");
             }
-
             T value = head.value;
             head = head.next;
-
             return value;
         }
 
         // push implementation
-        public void push(T value){
+        public void push(T value) {
             Node oldHead = this.head;
             this.head = new Node();
             this.head.value = value;
@@ -39,39 +39,40 @@ public class Assignment1 {
         }
 
         // print the stack from top to bottom
-        public void print(){
+        public void print() {
             System.out.print("(HEAD) ");
+            // store local copy of head here in print() function
+            Node n = head;
 
-            Node n = head; // store local copy of head here in print() function
-
-            if(head == null){
+            if (head == null) {
                 throw new CustomException("empty stack");
             }
 
-            while(n != null){
-                if(n.next != null) {
+            while (n != null) {
+                if (n.next != null) {
                     System.out.print(n.value + " -> ");
                 } else {
                     System.out.print(n.value + " ");
                 }
                 n = n.next;
             }
-
             System.out.println("(TAIL)");
         }
     }
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         // instantiate an instance of our stack backed by a linked list
         LinkedListStack<String> lls = new LinkedListStack<>();
-
-        // get a filesystem reference to the flat file to load; as per assignment1 instructions
+        // get a filesystem reference to the flat file to load; as per assignment1
+        // instructions
         Path path = Paths.get("ds20s-a1.txt");
-
-        // use the scanner class, in conjunction with a switch statement; to read the file line by line and determine the resultant set of data to be inserted into the stack
-        try (Scanner scanner = new Scanner(path)){
-            while(scanner.hasNextLine()){
+        // use the scanner class, in conjunction with a switch statement; to read the
+        // file line by line and determine the resultant set of data to be inserted into
+        // the stack
+        try (Scanner scanner = new Scanner(path)) {
+            while (scanner.hasNextLine()) {
                 String line = (scanner.nextLine()).trim();
-                switch(line){
+                switch (line) {
                     case "POP":
                         lls.pop();
                         break;
@@ -91,12 +92,10 @@ public class Assignment1 {
     static class CustomException extends RuntimeException {
         private static final long serialVersionUID = 1;
 
-        //
-        public CustomException(){
-
+        public CustomException() {
         }
 
-        public CustomException(String message){
+        public CustomException(String message) {
             super(message);
         }
     }

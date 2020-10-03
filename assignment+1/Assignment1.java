f23281c5798da86cf0a90dd7ed9d2c09286790ac
Sarabraj Singh <singh.sarabraj@gmail.com>
2020-10-02 23:27:24 -0400

    working implementation of assignment 1
    -> added dataloder from static file with logic
    -> added print() subroutine to ADT with visual representation of linkedlist

--- assignment+1/Assignment1.java
+++ assignment+1/Assignment1.java
@@ -1,13 +1,17 @@
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.Scanner;
+
 public class Assignment1 {
 
     static class LinkedListStack<T> {
         // an inner class to define a single linked list node
-        private class Node<T> {
+        private class Node {
             T value;
-            Node<T> next;
+            Node next;
         }
         // keep track of the head of the stack, head.next will point to the next element in the stack
-        private Node<T> head;
+        private Node head;
 
         // constructor for the stack class
         public LinkedListStack(){
@@ -28,47 +32,70 @@ public class Assignment1 {
 
         // push implementation
         public void push(T value){
-            Node<T> oldHead = this.head;
-            this.head = new Node<>();
+            Node oldHead = this.head;
+            this.head = new Node();
             this.head.value = value;
             this.head.next = oldHead;
         }
+
+        // print the stack from top to bottom
+        public void print(){
+            System.out.print("(HEAD) ");
+
+            Node n = head; // store local copy of head here in print() function
+
+            if(head == null){
+                throw new CustomException("empty stack");
+            }
+
+            while(n != null){
+                if(n.next != null) {
+                    System.out.print(n.value + " -> ");
+                } else {
+                    System.out.print(n.value + " ");
+                }
+                n = n.next;
+            }
+
+            System.out.println("(TAIL)");
+        }
     }
     public static void main(String[] args){
-
-        long startTime = System.currentTimeMillis();
-
+        // instantiate an instance of our stack backed by a linked list
         LinkedListStack<String> lls = new LinkedListStack<>();
 
-        lls.push("YO");
-        lls.push("WADDUP");
+        // get a filesystem reference to the flat file to load; as per assignment1 instructions
+        Path path = Paths.get("ds20s-a1.txt");
 
-        System.out.println("pop1=" + lls.pop());
-        System.out.println("pop2=" + lls.pop());
-        //System.out.println("pop3=" + lls.pop());
-
-        LinkedListStack<Integer> iLls = new LinkedListStack<>();
-        iLls.push(15);
-        iLls.push(20);
-        iLls.push(25);
-
-        System.out.println("pop4=" + iLls.pop());
-        System.out.println("pop5=" + iLls.pop());
-        System.out.println("pop6=" + iLls.pop());
-
-        long endTime = System.currentTimeMillis();
-
-        System.out.println(endTime-startTime);
+        // use the scanner class, in conjunction with a switch statement; to read the file line by line and determine the resultant set of data to be inserted into the stack
+        try (Scanner scanner = new Scanner(path)){
+            while(scanner.hasNextLine()){
+                String line = (scanner.nextLine()).trim();
+                switch(line){
+                    case "POP":
+                        lls.pop();
+                        break;
+                    case "PRINT":
+                        lls.print();
+                        System.out.println(); // insert extra new line for readability
+                        break;
+                    default:
+                        lls.push(line);
+                }
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
-    
+
     static class CustomException extends RuntimeException {
         private static final long serialVersionUID = 1;
-    
+
         //
         public CustomException(){
-    
+
         }
-    
+
         public CustomException(String message){
             super(message);
         }

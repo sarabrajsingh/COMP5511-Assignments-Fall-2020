4f4959ef022216d873ba61f1632969bb80eb285d
dunyaoguz <dunya.oguz@mail.mcgill.ca>
2020-10-27 00:42:48 -0400

    assignment 2 programming part

---
+++ assignment+2/Assignment2PQ1.java
@@ -0,0 +1,105 @@
+public class Assignment2PQ1 {
+
+  // A custom linked list class
+  public static class MyLinkedList<G> {
+
+    public static class Node<G> {
+      public G data;
+      public Node next;
+
+      // Constructor for the Node class
+      public Node (G data) {
+        this.data = data;
+        this.next = null;
+      }
+    }
+
+    // Class variable that represents the head Node at the top
+    public Node head;
+
+    // Constructor for the LinkedStack Class
+    public MyLinkedList() {
+      this.head = null;
+    }
+
+    public Node peek() {
+      return this.head;
+    }
+
+    public void add(G data) {
+      Node<G> new_node = new Node<>(data);
+
+      if (this.head == null) {
+        this.head = new_node;
+      } else {
+        Node next = this.head;
+        while (next.next != null) {
+          next = next.next;
+        }
+        next.next = new_node;
+      }
+    }
+
+    public void print() {
+      if (this.head == null) {
+        System.out.println("The Linked List doesn't contain any elements");
+      } else {
+        Node next = this.head;
+        while (next != null) {
+          System.out.println(next.data);
+          next = next.next;
+        }
+      }
+    }
+  }
+
+  public static int iterativeCount(MyLinkedList ll) {
+    MyLinkedList.Node head = ll.peek();
+
+    /* if head is null, linked list is empty,
+    therefore, return 0 */
+    if (head == null) {
+      return 0;
+
+    /* if head is not null, iterate through the
+    elements of the linked list */
+    } else {
+      MyLinkedList.Node next_element = head;
+      int count = 0;
+
+      while (next_element != null) {
+        next_element = next_element.next;
+        count++;
+      }
+      return count;
+    }
+  }
+
+  public static int recursiveCount(MyLinkedList.Node node) {
+    // Base case
+    if (node == null) {
+      return 0;
+    } else {
+      return 1 + recursiveCount(node.next);
+    }
+  }
+
+  public static void main(String args[]) {
+
+    // Create a LinkedList instance
+    MyLinkedList<Integer> ll = new MyLinkedList<>();
+
+    ll.add(1);
+    ll.add(2);
+    ll.add(3);
+    ll.add(4);
+    ll.add(5);
+    ll.add(6);
+
+    int c = iterativeCount(ll);
+    System.out.format("Iterative count yields: %s\n", c);
+
+    int c1 = recursiveCount(ll.peek());
+    System.out.format("Recursive count yields: %s\n", c1);
+  }
+}
---
+++ assignment+2/Assignment2PQ2.java
@@ -0,0 +1,217 @@
+import java.util.Stack;
+import java.util.Arrays;
+
+public class Assignment2PQ2 {
+
+  // a really basic Binary Tree Class
+  public static class BinaryTree {
+    public static class Node<G> {
+      public G data;
+      public Node left;
+      public Node right;
+
+      // constructor of Node
+      public Node(G data) {
+        this.data = data;
+        this.right = null;
+        this.left = null;
+      }
+
+      public Node getRight() {
+        return this.right;
+      }
+
+      public Node getLeft() {
+        return this.left;
+      }
+
+      public boolean isParent() {
+        if (this.right == null & this.left == null) {
+          return false;
+        } else {
+          return true;
+        }
+      }
+    }
+
+    public Node root;
+
+    // constructor of BinaryTree
+    public BinaryTree() {
+      this.root = null;
+    }
+
+    public void rightInsert(Node new_node, Node parent) {
+      // if tree is empty, set the node as root
+      if (this.root == null) {
+        this.root = new_node;
+      /* otherwise, insert the new node on the right */
+      } else {
+        Node rightChild = parent.getRight();
+
+        if (rightChild == null) {
+          parent.right = new_node;
+        } else {
+          System.out.println("Couldn\'t insert this node to the Binary tree because the parent already has a right child");
+        }
+      }
+    }
+
+    public void leftInsert(Node new_node, Node parent) {
+      // if tree is empty, set the node as root
+      if (this.root == null) {
+        this.root = new_node;
+      /* otherwise, insert the new node on the right */
+      } else {
+        Node leftChild = parent.getLeft();
+
+        if (leftChild == null) {
+          parent.left = new_node;
+        } else {
+          System.out.println("Couldn\'t insert this node to the Binary tree because the parent already has a left child");
+        }
+      }
+    }
+
+    // print the tree using in order traversal
+    public void printTree() {
+      Stack<Node> s = new Stack<Node>();
+      Node current = this.root;
+
+      if (current == null) {
+        System.out.println("Tree is empty.");
+        return;
+      }
+
+      // traverse the tree
+      while (current != null || s.size() > 0) {
+          // reach the left most Node of the current Node
+          while (current !=  null) {
+              s.push(current);
+              current = current.left;
+          }
+          // current must be NULL at this point
+          current = s.pop();
+          System.out.print(current.data + " ");
+          // now, visit the right subtree
+          current = current.right;
+      }
+      // print a new line when we reach the end of the tree
+      System.out.println("");
+    }
+  }
+
+  public static Object evaluateNode(BinaryTree.Node n) {
+    BinaryTree.Node rightChild = n.getRight();
+    BinaryTree.Node leftChild = n.getLeft();
+
+    if (leftChild.isParent() & rightChild.isParent()) {
+      return 1;
+    } else if (rightChild.isParent()) {
+      return 2;
+    } else if (leftChild.isParent()) {
+      return 3;
+    } else {
+      return 4;
+    }
+  }
+
+  public static Stack createStack(BinaryTree bt) {
+    Stack<Object> evaluationStack = new Stack<>();
+    Stack<BinaryTree.Node> traversalStack = new Stack<BinaryTree.Node>();
+
+    BinaryTree.Node current = bt.root;
+
+    if (current == null) {
+      System.out.println("There is no arithmetic operation to evaluate on the given tree.");
+    }
+
+    while (current != null || traversalStack.size() > 0) {
+        // reach the left most Node of the current Node
+        while (current != null) {
+            traversalStack.push(current);
+            evaluationStack.push(current.data);
+            current = current.left;
+        }
+        // current must be NULL at this point
+        current = traversalStack.pop();
+        // now, visit the right subtree
+        current = current.right;
+    }
+    return evaluationStack;
+  }
+
+  public static Integer solvePostfix(Stack s) {
+    Stack<Object> temp = new Stack<>();
+    Integer size = s.size();
+
+    for (Integer i=0; i<size; i++) {
+      while (s.size() > 0) {
+        Object element = s.pop();
+
+        if (element instanceof Integer) {
+          temp.push(element);
+        } else {
+          Integer o1 = (Integer) temp.pop();
+          Integer o2 = (Integer) temp.pop();
+          Integer result = null;
+
+          if (element == "+") {
+            result = o1 + o2;
+          } else if (element == "-") {
+            result = o1 - o2;
+          } else if (element == "\\") {
+            result = o1 / o2;
+          } else if (element == "x") {
+            result = o1 * o2;
+          }
+
+          temp.push(result);
+        }
+      }
+    }
+    Integer final_result = (Integer) temp.pop();
+    return final_result;
+  }
+
+  public static void main(String args[]) {
+    BinaryTree bt = new BinaryTree();
+
+    // Populate the binary tree
+    BinaryTree.Node<String> r0 = new BinaryTree.Node<>("+");
+    bt.rightInsert(r0, null);
+
+    BinaryTree.Node<String> r1 = new BinaryTree.Node<>("x");
+    bt.leftInsert(r1, r0);
+
+    BinaryTree.Node<String> r2 = new BinaryTree.Node<>("x");
+    bt.rightInsert(r2, r0);
+
+    BinaryTree.Node<Integer> r3 = new BinaryTree.Node<>(2);
+    bt.leftInsert(r3, r1);
+
+    BinaryTree.Node<String> r4 = new BinaryTree.Node<>("-");
+    bt.rightInsert(r4, r1);
+
+    BinaryTree.Node<Integer> r5 = new BinaryTree.Node<>(5);
+    bt.leftInsert(r5, r4);
+
+    BinaryTree.Node<Integer> r6 = new BinaryTree.Node<>(1);
+    bt.rightInsert(r6, r4);
+
+    BinaryTree.Node<Integer> r7 = new BinaryTree.Node<>(3);
+    bt.leftInsert(r7, r2);
+
+    BinaryTree.Node<Integer> r8 = new BinaryTree.Node<>(2);
+    bt.rightInsert(r8, r2);
+
+    // Peek at the tree, using inorder traversal
+    bt.printTree();
+
+    Stack s = createStack(bt);
+    System.out.println(Arrays.toString(s.toArray()));
+
+    Integer result = solvePostfix(s);
+    System.out.println(result);
+  }
+}

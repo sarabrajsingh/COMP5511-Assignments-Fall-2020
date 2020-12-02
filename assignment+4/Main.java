a5da39c2a27122bbdb88fde32bcde04b004d8918
dunyaoguz <dunya.oguz@mail.mcgill.ca>
2020-12-02 02:26:49 -0500

    create method for reading Queries

--- assignment+4/Main.java
+++ assignment+4/Main.java
@@ -11,12 +11,17 @@ import java.util.*;
 
 public class Main {
 
-    // reads a given csv file line by line, returns an arraylist of records
-    public static int lineCounter(String filename) throws IOException {
+    // returns a BufferedReader object for a given file
+    public static BufferedReader fileReader(String filename) throws IOException {
       File file = new File(filename);
       FileReader fr = new FileReader(file);
       BufferedReader br = new BufferedReader(fr);
+      return br;
+    }
 
+    // counts the number of rows in a given csv
+    public static int lineCounter(String filename) throws IOException {
+      BufferedReader br = fileReader(filename);
       int lineCount = 0;
       while (br.readLine() != null) {
         lineCount++;
@@ -24,46 +29,53 @@ public class Main {
       return lineCount;
     }
 
+    // reads a given csv file line by line and returns an array of records
     public static Record[] readCSV(String filename, int lineCount) throws IOException {
-      File file = new File(filename);
-      FileReader fr = new FileReader(file);
-      BufferedReader br = new BufferedReader(fr);
-
-      // initialize an array that will store the collection of all record objects
+      BufferedReader br = fileReader(filename);
       Record[] records = new Record[lineCount-1];
-
       // skip first row
       br.readLine();
       String row;
       int c = 0;
-
       while((row = br.readLine()) != null) {
         Record r = new Record(row);
         records[c] = r;
-
-        // print every 1000th record for demonstration purposes
-        if (c%1000 == 0) {
+        // print every 10000th record for demonstration purposes
+        if (c%10000 == 0) {
           r.printRecord();
         }
         c++;
       }
       br.close();
-
       return records;
     }
 
+  // reads the .txt file that stores the queries
+  public static Query readQueries(String filename) throws IOException {
+    BufferedReader br = fileReader(filename);
+    String line;
+    String[] queries = new String[6];
+    int c = 0;
+    while ((line = br.readLine()) != null) {
+      queries[c] = line.split(":")[1];
+      c++;
+    }
+    Query q = new Query(queries);
+    q.printQuery();
+    return q;
+  }
+
   public static void main(String[] args) throws IOException {
-    System.out.println("READING FILE AND PRINTING SOME EXAMPLE RECORDS FOR DEMONSTRATION...");
-    // choose the file to work with
-    String filename = "data/cgn_yt_csv_eng.csv";
-
-    // count lines and instantiate a record array of the same size
-    int lineCount = lineCounter(filename);
-    System.out.printf("File chosen: %s \n", filename.split("/")[1]);
-    System.out.printf("Total records: %s \n", lineCount);
-    Record[] csv = new Record[lineCount];
+    System.out.println("\nREADING FILE AND PRINTING SOME EXAMPLE RECORDS FOR DEMONSTRATION...");
     System.out.println("===========================");
+    // choose the file to work with
+    String filename = "data/cgn_on_csv_eng.csv";
+    // display some info about the chosen file
+    int lineCount = lineCounter(filename);
+    System.out.printf("\nFile chosen: %s \n", filename.split("/")[1]);
+    System.out.printf("Total records: %s \n\n", lineCount);
 
+    Record[] csv = new Record[lineCount];
     try {
       csv = readCSV(filename, lineCount);
     } catch(FileNotFoundException e) {
@@ -83,27 +95,55 @@ public class Main {
     }
 
     // create an Inverted Index for Generic Term Lookup
+    InvertedIndex inv2 = new InvertedIndex();
+    for (Record r : csv) {
+      inv2.put(r.genericTerm, r);
+    }
+
+    // create an Inverted Index for Location Lookup
+    InvertedIndex inv3 = new InvertedIndex();
+    for (Record r : csv) {
+      inv3.put(r.location, r);
+    }
 
     // create a B Tree for Lat/Long Lookup
-
-    System.out.println();
-    System.out.println("READING QUERIES...");
+    System.out.println("\nREADING QUERIES AND SAVING RESULTS TO LOG FILE...");
     System.out.println("===========================");
+    Query q = new Query();
 
     try {
-      File file = new File("log_files/queries.txt");
-      Scanner sc = new Scanner(file);
-      sc.useDelimiter("\n");
-      System.out.println(sc.next());
-      while(sc.hasNext()){
-        System.out.println(sc.next());
-      }
-      sc.close();
+      q = readQueries("log_files/queries.txt");
     } catch(FileNotFoundException e) {
       System.out.println("File was not found.");
     }
 
     // Perform the searches
+    if (q.cgndbId != null) {
+      for(String id : q.cgndbId) {
+        System.out.printf("Querying %s: ", id);
+        bs.search(id);
+      }
+    }
 
+    if (q.geographicName != null) {
+      for(String n : q.geographicName) {
+        System.out.printf("Querying %s: ", n);
+        inv.get(n);
+      }
+    }
+
+    if (q.genericTerm != null) {
+      for(String g : q.genericTerm) {
+        System.out.printf("Querying %s: ", g);
+        inv2.get(g);
+      }
+    }
+
+    if (q.location != null) {
+      for(String l : q.location) {
+        System.out.printf("Querying %s: ", l);
+        inv3.get(l);
+      }
+    }
   }
 }
--- assignment+4/Query.java
+++ assignment+4/Query.java
@@ -8,13 +8,22 @@ public class Query {
   public ArrayList<String> genericTerm = new ArrayList<>();
   public ArrayList<Float> latitude = new ArrayList<>();
   public ArrayList<Float> longitude = new ArrayList<>();
-  public ArrayList<String> province = new ArrayList<>();
+  public ArrayList<String> location = new ArrayList<>();
+
+  // constructor an empty Query object, for declaration
+  public Query() {
+    this.cgndbId = null;
+    this.genericTerm = null;
+    this.latitude = null;
+    this.longitude = null;
+    this.location = null;
+  }
 
   // constructor for Query object
   public Query(String[] query) {
 
     for(String id : query[0].split(",")) {
-      if (id != "None") {
+      if (!id.trim().equals("None"))  {
         this.cgndbId.add(id.trim());
       } else {
         this.cgndbId = null;
@@ -22,7 +31,7 @@ public class Query {
     }
 
     for(String n : query[1].split(",")) {
-      if (n != "None") {
+      if (!n.trim().equals("None"))  {
         this.geographicName.add(n.trim());
       } else {
         this.geographicName = null;
@@ -30,7 +39,8 @@ public class Query {
     }
 
     for(String t : query[2].split(",")) {
-      if (t != "None") {
+      if (!t.trim().equals("None")) {
+        System.out.println(t.trim());
         this.genericTerm.add(t.trim());
       } else {
         this.genericTerm = null;
@@ -38,7 +48,7 @@ public class Query {
     }
 
     for(String f : query[3].split(",")) {
-      if (f != "None") {
+      if (!f.trim().equals("None"))  {
         Float l = Float.parseFloat(f.trim());
         this.latitude.add(l);
       } else {
@@ -47,7 +57,7 @@ public class Query {
     }
 
     for(String f : query[4].split(",")) {
-      if (f != "None") {
+      if (!f.trim().equals("None"))  {
         Float l = Float.parseFloat(f.trim());
         this.longitude.add(l);
       } else {
@@ -55,23 +65,36 @@ public class Query {
       }
     }
 
-    for(String p : query[5].split(",")) {
-      if (p != "None") {
-        this.province.add(p.trim());
+    for(String l : query[5].split(",")) {
+      if (!l.trim().equals("None"))  {
+        this.location.add(l.trim());
       } else {
-        this.province = null;
+        this.location = null;
       }
     }
   }
 
   // print the info of a query
   public void printQuery() {
-    System.out.printf("|| CGNDBID: %s ---> ", this.cgndbId);
-    System.out.printf("Geographic Name: %s | ", this.geographicName);
-    System.out.printf("Generic Term: %s | ", this.genericTerm);
-    System.out.printf("Latitude: %s | ", this.latitude);
-    System.out.printf("Longitude: %s | ", this.longitude);
-    System.out.printf("Province: %s ||\n", this.province);
+    if (this.cgndbId != null) {
+      System.out.printf("CGNDBID(s): %s \n", this.cgndbId);
+    }
+
+    if (this.geographicName != null) {
+      System.out.printf("Geographic Name(s): %s \n", this.geographicName);
+    }
+
+    if (this.genericTerm != null) {
+      System.out.printf("Generic Term(s): %s \n", this.genericTerm);
+    }
+
+    if (this.latitude != null & this.longitude != null) {
+      System.out.printf("Latitude(s) & Longitude(s): [%s, %s]\n", this.latitude, this.longitude);
+    }
+
+    if (this.location != null) {
+      System.out.printf("Location(s): %s \n\n", this.location);
+    }
   }
 
   public static void main(String[] args) {
